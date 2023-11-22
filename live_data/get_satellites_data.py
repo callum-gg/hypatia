4e836358d89c941503c88d592d96219763765486
Callum Gray <callumgray.cg@gmail.com>
2023-11-22 13:17:44 +0000

    Get live satellite data

---
+++ live_data/get_satellites_data.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from skyfield.sgp4lib import EarthSatellite
+from skyfield.api import load
+from skyfield.toposlib import wgs84
+
+# Load .env file
+load_dotenv()
+
+# API URLs
+base_url = "https://www.space-track.org"
+request_login = "/ajaxauth/login"
+
+# Log into session using credentials
+session = requests.Session()
+resp = session.post(base_url + request_login, data = {
+    "identity": os.getenv("EMAIL"),
+    "password": os.getenv("PASSWORD")
+})
+# Check session has successfully been logged into
+logged_in = False
+if resp.status_code != 200 or resp.text == "{\"Login\":\"Failed\"}":
+    print("POST fail on login")
+    session.close()
+else:
+    logged_in = True
+
+def GetSatellitesData():
+    if not logged_in:
+        return
+    # Could change to only return necessary data (speed up requests?)
+    # resp = session.get("https://www.space-track.org/basicspacedata/query/class/gp/OBJECT_NAME/STARLINK-%5E")
+    resp = session.get("https://www.space-track.org/basicspacedata/query/class/gp/OBJECT_NAME/STARLINK-%5E/limit/1")
+
+    # Error handling
+    satellites = json.loads(resp.text)
+
+    # Generate positional information
+    time_now = load.timescale().now()
+    for i in range(0, len(satellites)):
+        # Need to adjust for time data was taken from
+        # And adjust current values as well
+        satellite = EarthSatellite(satellites[i]["TLE_LINE1"], satellites[i]["TLE_LINE2"])#, ts=)
+        geocentric = satellite.at(time_now)
+        lat, lon = wgs84.latlon_of(geocentric)
+        satellites[i]["LATITUDE"], satellites[i]["LONGITUDE"] = lat.degrees, lon.degrees
+
+    # Debug information
+    # print("Number of Starlink satellites:", len(satellites))
+    # print("Example Satellite:")
+    # print(json.dumps(satellites[0]))
+
+    # Process data for correct output
+
+    return satellites
+
+if __name__ == "__main__":
+    GetSatellitesData()
+
+# Close session
+# Might cause issues?
+session.close()
---
+++ live_data/test.py
@@ -0,0 +1,48 @@
+# Setting home directory for running
+import os
+os.chdir("C:/Users/callu/Documents/hypatia")
+
+import sys
+sys.path.append("satviz/scripts")
+
+from get_satellites_data import GetSatellitesData
+
+def GenerateSatelliteJavascript(satellites):
+    viz_string = ""
+    for j in range(len(satellites)):
+        viz_string += "var redSphere = viewer.entities.add({name : '', position: Cesium.Cartesian3.fromDegrees(" \
+                        + str(satellites[j]["LONGITUDE"]) + ", " \
+                        + str(satellites[j]["LATITUDE"]) + ", " + str(
+            (float(satellites[j]["APOAPSIS"]) + float(satellites[j]["APOAPSIS"])) * 500) + "), " \
+                        + "ellipsoid : {radii : new Cesium.Cartesian3(30000.0, 30000.0, 30000.0), " \
+                        + "material : Cesium.Color.BLACK.withAlpha(1),}});\n"
+
+    # for key in orbit_links:
+    #     sat1 = orbit_links[key]["sat1"]
+    #     sat2 = orbit_links[key]["sat2"]
+    #     viz_string += "viewer.entities.add({name : '', polyline: { positions: Cesium.Cartesian3.fromDegreesArrayHeights([" \
+    #                     + str(math.degrees(sat_objs[sat1]["sat_obj"].sublong)) + "," \
+    #                     + str(math.degrees(sat_objs[sat1]["sat_obj"].sublat)) + "," \
+    #                     + str(sat_objs[sat1]["alt_km"] * 1000) + "," \
+    #                     + str(math.degrees(sat_objs[sat2]["sat_obj"].sublong)) + "," \
+    #                     + str(math.degrees(sat_objs[sat2]["sat_obj"].sublat)) + "," \
+    #                     + str(sat_objs[sat2]["alt_km"] * 1000) + "]), " \
+    #                     + "width: 0.5, arcType: Cesium.ArcType.NONE, " \
+    #                     + "material: new Cesium.PolylineOutlineMaterialProperty({ " \
+    #                     + "color: Cesium.Color."+COLOR[i]+".withAlpha(0.4), outlineWidth: 0, outlineColor: Cesium.Color.BLACK})}});"
+
+    return viz_string
+
+# Get satellites data
+satellites = GetSatellitesData()
+
+# HTML output file writer
+output_file = open("satviz/viz_output/output.html", "w")
+with open("satviz/static_html/top.html", "r") as top_html:
+    output_file.write(top_html.read())
+
+# Write extra script code for satellites
+output_file.write(GenerateSatelliteJavascript(satellites))
+
+with open("satviz/static_html/bottom.html", "r") as bottom_html:
+    output_file.write(bottom_html.read())
