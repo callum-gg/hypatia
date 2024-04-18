74a64ca44cef218abf396579e73de98960d06c27
callum-gg <callumgray.cg@gmail.com>
2024-04-18 19:54:14 +0100

    Added general live data

---
+++ paper/satellite_networks_state/main_live.py
@@ -0,0 +1,56 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 ETH Zurich
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+from main_helper import LiveHelper
+
+def main():
+    args = sys.argv[1:]
+    args = ["ONEWEB", "2", "1000", "isls_none", "ground_stations_top_100", "algorithm_free_one_only_over_isls", "1"]
+    if len(args) != 7:
+        print("Must supply exactly six arguments")
+        print("Usage: python main_live.py [constellation name] [duration (s)] [time step (ms)] "
+              "[isls_plus_grid / isls_none] "
+              "[ground_stations_{top_100, paris_moscow_grid}] "
+              "[algorithm_{free_one_only_over_isls, free_one_only_gs_relays, paired_many_only_over_isls}] "
+              "[num threads]")
+        exit(1)
+    else:
+        live_helper = LiveHelper(
+            args[0].lower(),
+            args[0].title(),
+            args[0].upper(),
+        )
+
+        live_helper.calculate(
+            "gen_data",
+            int(args[1]),
+            int(args[2]),
+            args[3],
+            args[4],
+            args[5],
+            int(args[6])
+        )
+
+
+if __name__ == "__main__":
+    main()
